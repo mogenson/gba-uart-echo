189b68fb4633b2792063f3f212f8373ed707f769
Michael Mogenson <michael.mogenson@gmail.com>
2020-10-20 16:22:15 -0400

    panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 #![no_std]
 #![no_main]
 
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unsafe {
-        let x = 0_i32;
-        loop {
-            (&x as *const i32).read_volatile();
-        }
-    }
-}
+use panic_abort as _;
 
 #[no_mangle]
 pub fn main() {
